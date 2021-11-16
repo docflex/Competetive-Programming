52324eb1c27cc81f98009ad5018867a55b9622f5
docflex <rehbermoin.io@gmail.com>
2021-11-16 14:33:25 +0530

    Testing If Testing Reflects

--- Testing/SourceCode.cpp
+++ Testing/SourceCode.cpp
@@ -92,6 +92,7 @@ ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n
 // clang-format on
 void solveMyProblem(istream &cin, ostream &cout) {
     // Solve Here
+    cout << "hello" << el;
 }
 
 int32_t main() {
